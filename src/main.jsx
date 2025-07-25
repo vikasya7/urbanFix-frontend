923902fe8648f0157d5bd3907043aa7d4a55eaaa
vikasya7 <vikasyaa19@gmail.com>
2025-07-25 12:12:08 +0530

    Wrap App component with AuthProvider for improved authentication context

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
+// main.jsx or index.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext' // ✅ import this
 import 'leaflet/dist/leaflet.css';
 
 createRoot(document.getElementById('root')).render(
-
-     <BrowserRouter>
-    <App />
-  </BrowserRouter>
-
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider> {/* ✅ Add this */}
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
+
